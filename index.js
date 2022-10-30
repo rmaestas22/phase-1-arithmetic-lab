c5866ac659ca1d9e44703904181cbd731b239002
Ray <rjmaestas1991@gmail.com>
2022-10-30 15:47:37 -0600

    commit tree 2fea0e16b9e641e1f79f0abff50e7a98c58b1788

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply;
+multiply = num1 * num2;
+multiply = 62;
+
+let random = Math.floor(Math.random() * 11);
+
+let num3 = 10
+let num4 = 6
+ let mod= num3-num4
+
+let max= Math.max(3, 15, 10, 20, 8, 18);
